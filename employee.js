5cd8d543bf70fa61eba874a4f8b330beb16eed33
curtis-culpepper <107226325+curtis-culpepper@users.noreply.github.com>
2022-08-03 19:40:00 -0400

    part 1 done

--- employee.js
+++ employee.js
@@ -1,1 +1,16 @@
-//--------
+class Employee {
+    constructor(name, salary, title, manager = null) {
+        this.name = name;
+        this.salary = salary;
+        this.title = title;
+        this.manager = manager;
+    }
+
+
+
+
+}
+
+module.exports = Employee;
+
+
